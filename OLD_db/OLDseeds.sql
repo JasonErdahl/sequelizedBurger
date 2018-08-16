b80c6f9ff092164227c579b6b2653b88ee53b5db
Jason Erdahl <jasonerdahl@gmail.com>
2018-08-16 16:44:04 -0500

    sequelized Init

---
+++ OLD_db/OLDseeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name) VALUES ('Steak Burger');
+INSERT INTO burgers (burger_name) VALUES ('Patty Melt');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chili Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', true);
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Whopper');
